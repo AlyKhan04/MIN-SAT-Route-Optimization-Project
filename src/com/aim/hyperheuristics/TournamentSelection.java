049580142f87e5cca0ec264b67b90eabcdd1a305
AlyKhan04 <alykhan222004@gmail.com>
2024-05-10 02:36:24 +0100

    oprimizign

--- src/com/aim/hyperheuristics/TournamentSelection.java
+++ src/com/aim/hyperheuristics/TournamentSelection.java
@@ -7,6 +7,7 @@ import com.aim.UZFDomain;
 import com.aim.interfaces.UAVSolutionInterface;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class TournamentSelection extends HyperHeuristic  {
     private static final int SECOND_PARENT_INDEX = 2;
@@ -20,67 +21,69 @@ public class TournamentSelection extends HyperHeuristic  {
 
     @Override
     protected void solve(ProblemDomain oProblem) {
-        //sets the size of memory
+        // Set memory size to 5 slots (0 through 4)
         oProblem.setMemorySize(4);
-        //creates an array with the number of heuristics
+
+        // Initialize arrays and indices
         int numberOfHeuristics = oProblem.getNumberOfHeuristics();
         int[] heuristicScores = new int[numberOfHeuristics];
         Arrays.fill(heuristicScores, 1);
-        //creates two indices with the solution intiialized and a copy solution function created
-        int currentIndex = 0;
-        int candidateIndex = 1;
-        //Initialises the solution
+        int currentIndex = 0, candidateIndex = 1, bestIndex = 3;
+
+        // Initialize the solution and set the best solution index
         oProblem.initialiseSolution(currentIndex);
-        oProblem.copySolution(currentIndex, BEST_ACCEPTED_INDEX);
-        //brings the original solution cost
-        double currentCost = oProblem.getFunctionValue(currentIndex);
-        // Creates boolean array to identify crossover Heuristics
+        oProblem.copySolution(currentIndex, bestIndex);
+        double bestCost = oProblem.getFunctionValue(currentIndex);
+        double currentCost = bestCost;
+
+        // Identify crossover heuristics
         boolean[] isCrossover = new boolean[numberOfHeuristics];
         Arrays.fill(isCrossover, false);
-        //sets a boolean array to true for each array with which crossover is true
-        for(int i : oProblem.getHeuristicsOfType(ProblemDomain.HeuristicType.CROSSOVER)) {
+        for (int i : oProblem.getHeuristicsOfType(ProblemDomain.HeuristicType.CROSSOVER)) {
             isCrossover[i] = true;
         }
-        // main search loop
+
+        // Main optimization loop
         double candidateCost;
-        //Creates a while loop
-        while(!hasTimeExpired()) {
-            //Creates a random tournament size
+        while (!hasTimeExpired()) {
             int tournamentSize = rng.nextInt(numberOfHeuristics);
-            //Creates a permutation
             int[] tournament = createRandomPermutation(numberOfHeuristics);
             int h = tournament[0];
-            //Chooses a heuristic that returns h with the best tournment score
             int bestscore = heuristicScores[tournament[0]];
             for (int i = 0; i < tournamentSize; i++) {
                 if (heuristicScores[tournament[i]] < bestscore) {
-                    bestscore = heuristicScores[i];
-                    h = i;
+                    bestscore = heuristicScores[tournament[i]];
+                    h = tournament[i];
                 }
             }
-            //If the heuristic is a crossover heuristic
-            if(isCrossover[h]) {
-                if(rng.nextBoolean()) {
-                    // randomly choose between crossover with newly initialised solution
-                    oProblem.initialiseSolution(SECOND_PARENT_INDEX);
-                    candidateCost = oProblem.applyHeuristic(h, currentIndex, SECOND_PARENT_INDEX, candidateIndex);
+
+            // Apply heuristic
+            if (isCrossover[h]) {
+                if (rng.nextBoolean()) {
+                    oProblem.initialiseSolution(2);  // Use index 2 for the second parent
+                    candidateCost = oProblem.applyHeuristic(h, currentIndex, 2, candidateIndex);
                 } else {
-                    // or with best solution accepted so far
-                    candidateCost = oProblem.applyHeuristic(h, currentIndex, BEST_ACCEPTED_INDEX, candidateIndex);
+                    candidateCost = oProblem.applyHeuristic(h, currentIndex, bestIndex, candidateIndex);
                 }
             } else {
                 candidateCost = oProblem.applyHeuristic(h, currentIndex, candidateIndex);
             }
 
-            // Evaluation Function
-            if(candidateCost < currentCost) {
-                oProblem.copySolution(candidateIndex, BEST_ACCEPTED_INDEX);
+            // Update best solution if found
+            if (candidateCost < bestCost) {
+                oProblem.copySolution(candidateIndex, bestIndex);
+                bestCost = candidateCost;
             }
-            if (candidateCost <= currentCost){
+
+            // Swap indices for the next iteration
+            int temp = currentIndex;
+            currentIndex = candidateIndex;
+            candidateIndex = temp;
+
+            // Update heuristic scores
+            if (candidateCost <= currentCost) {
                 heuristicScores[h]++;
                 currentCost = candidateCost;
-                currentIndex = 1 - currentIndex;
-                candidateIndex = 1 - candidateIndex;
             } else {
                 heuristicScores[h]--;
             }
@@ -91,13 +94,17 @@ public class TournamentSelection extends HyperHeuristic  {
         oSolutionPrinter.printSolution( ((UZFDomain) oProblem).getLoadedInstance().getSolutionAsListOfLocations(oSolution));
     }
 
-    public int[] createRandomPermutation (int length) {
-        int[] perm = new int[length];
+    public int[] createRandomPermutation(int length) {
+        Integer[] perm = new Integer[length];
         for (int i = 0; i < length; i++) {
             perm[i] = i;
         }
-        java.util.Collections.shuffle(Arrays.asList(perm));
-        return perm;
+        Collections.shuffle(Arrays.asList(perm));
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = perm[i];
+        }
+        return result;
     }
 
     @Override
--- src/com/aim/runners/SR_IE_VisualRunner.java
+++ src/com/aim/runners/SR_IE_VisualRunner.java
@@ -3,12 +3,9 @@ package com.aim.runners;
 
 import Examples.ExampleHyperHeuristic1;
 //import com.aim.hyperheuristics.GreedySearch;
-import com.aim.hyperheuristics.GreedySearch;
-import com.aim.hyperheuristics.ModifiedChoiceFunctionAllMoves;
-import com.aim.hyperheuristics.SR_IE_HH;
+import com.aim.hyperheuristics.*;
 
 import AbstractClasses.HyperHeuristic;
-import com.aim.hyperheuristics.SingleHeuristicTester;
 
 /**
  * @author Warren G Jackson
@@ -24,12 +21,12 @@ public class SR_IE_VisualRunner extends HH_Runner_Visual {
 	@Override
 	protected HyperHeuristic getHyperHeuristic(long seed) {
 
-		return new SR_IE_HH(seed);
+		return new TournamentSelection(seed);
 	}
 	
 	public static void main(String [] args) {
 		
-		HH_Runner_Visual runner = new SR_IE_VisualRunner(1);
+		HH_Runner_Visual runner = new SR_IE_VisualRunner(4);
 		runner.run();
 	}
 
